a7c0834c098fffee31e1fe5fc33232d194fe35c1
JKChang2015 <jkchang2015@gmail.com>
2018-04-10 09:38:54 +0100

    between markers

--- Checkio/between_markers.py
+++ Checkio/between_markers.py
@@ -20,16 +20,13 @@ def between_markers(text: str, begin: str, end: str) -> str:
     """
 
 
-
-
 if __name__ == '__main__':
     print('Example:')
     print(between_markers('What is >apple<', '>', '<'))
 
     # These "asserts" are used for self-checking and not for testing
     assert between_markers('What is >apple<', '>', '<') == "apple", "One sym"
-    assert between_markers("<head><title>My new site</title></head>",
-                           "<title>", "</title>") == "My new site", "HTML"
+    assert between_markers("<head><title>My new site</title></head>", "<title>", "</title>") == "My new site", "HTML"
     assert between_markers('No[/b] hi', '[b]', '[/b]') == 'No', 'No opened'
     assert between_markers('No [b]hi', '[b]', '[/b]') == 'hi', 'No close'
     assert between_markers('No hi', '[b]', '[/b]') == 'No hi', 'No markers at all'
