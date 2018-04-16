8f8729b41a55f50e4ac528774b7c51c5dbef2658
JKChang2015 <jkchang2015@gmail.com>
2018-04-16 17:13:20 +0100

    ?

--- Checkio/right_to_left.py
+++ Checkio/right_to_left.py
@@ -20,7 +20,8 @@ def left_join(phrases):
         Join strings and replace "right" to "left"
     """
     # l = list(phrases)
-    return ','.join([x.replace('right','left') for x in phrases])
+    return ','.join([x.replace('right', 'left') for x in phrases])
+
 
 if __name__ == '__main__':
     # These "asserts" using only for self-checking and not necessary for auto-testing
