fb679ec5eb08716f0652d08518df400b0aae0b1c
JKChang2015 <jkchang2015@gmail.com>
2019-08-12 11:19:31 +0100

    update

--- Checkio/bird_language.py
+++ Checkio/bird_language.py
@@ -31,17 +31,26 @@
 
 VOWELS = "aeiouy"
 
+
 def translate(phrase):
+    VOWELS = "aeiouy"
+    position = 0
+    while position < len(phrase):
+        if phrase[position] in VOWELS:
+            phrase = phrase[:position + 1] + phrase[position + 3:]
+        elif phrase[position] != ' ':
+            phrase = phrase[:position + 1] + phrase[position + 2:]
+        position += 1
     return phrase
+
 
 if __name__ == '__main__':
     print("Example:")
     print(translate("hieeelalaooo"))
 
-    #These "asserts" using only for self-checking and not necessary for auto-testing
+    # These "asserts" using only for self-checking and not necessary for auto-testing
     assert translate("hieeelalaooo") == "hello", "Hi!"
     assert translate("hoooowe yyyooouuu duoooiiine") == "how you doin", "Joey?"
     assert translate("aaa bo cy da eee fe") == "a b c d e f", "Alphabet"
     assert translate("sooooso aaaaaaaaa") == "sos aaa", "Mayday, mayday"
     print("Coding complete? Click 'Check' to review your tests and earn cool rewards!")
-
