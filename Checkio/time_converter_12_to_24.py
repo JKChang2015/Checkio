a96fcf866c7b766224927eeaedc7dc5661924800
JKChang2015 <jkchang2015@gmail.com>
2019-08-15 14:28:30 +0100

    beauti

---
+++ Checkio/time_converter_12_to_24.py
@@ -0,0 +1,48 @@
+# time_converter_12_to_24.py
+# Created by JKChang
+# 2019-08-15, 12:05
+# Tag:
+# Description: You are the modern man who prefers the 24-hour time format. But the 12-hour format is used in some
+# places. Your task is to convert the time from the 12-h format into 24-h by following the next rules:
+# - the output format should be 'hh:mm'
+# - if the output hour is less than 10 - write '0' before it. For example: '09:05'
+# Here you can find some useful information about the 12-hour format.
+#
+# example
+#
+# Input: Time in a 12-hour format (as a string).
+#
+# Output: Time in a 24-hour format (as a string).
+# Example:
+#
+# time_converter('12:30 p.m.') == '12:30'
+# time_converter('9:00 a.m.') == '09:00'
+# time_converter('11:15 p.m.') == '23:15'
+
+def time_converter(time):
+    t, sign = time.split(' ')
+    hour, mini = t.split(':')
+
+    if sign == 'a.m.':
+        if int(hour) < 10:
+            return '0' + t
+        elif hour == '12':
+            return '00' + ':' + mini
+        else:
+            return time
+    else:
+        if hour == '12':
+            return t
+        else:
+            return str(int(hour) + 12) + ':' + mini
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(time_converter('12:30 p.m.'))
+
+    # These "asserts" using only for self-checking and not necessary for auto-testing
+    assert time_converter('12:30 p.m.') == '12:30'
+    assert time_converter('9:00 a.m.') == '09:00'
+    assert time_converter('11:15 p.m.') == '23:15'
+    print("Coding complete? Click 'Check' to earn cool rewards!")
